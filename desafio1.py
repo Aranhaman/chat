5fd75c87ae35bff2e2f1fc5d813f9727f5d50a69
Robert Rios <Robert3d_@hotmail.com>
2023-08-23 20:27:38 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,9 @@
+#funções que ira necessita dos numeros
+a = int(input('coloque o primeiro numero:'))
+b = int(input('coloque o sengundo numero:'))
+
+# função que soma os numeros
+c = a + b
+
+#apresetação do resultado
+print('resultado da soma dos numeros:',format(c))
